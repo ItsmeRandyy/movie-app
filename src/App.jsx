6a0223d4053c7edee46e22a95863957a6f36b4e2
ItsmeRandyy <139468401+ItsmeRandyy@users.noreply.github.com>
2025-07-20 13:28:26 +0200

    database implemented

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Search from './components/search.jsx';
 import Spinner from './components/Spinner.jsx';
 import MovieCard from './components/MovieCard.jsx';
 import { useDebounce } from 'react-use';
+import { updateSearchCount } from './appwrite.js';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -37,6 +38,10 @@ const fetchMovies = async (query = '') => {
 
     const data = await response.json();
     setMovieList(data.results || []);
+
+    if (query && data.results.length > 0) {
+      await updateSearchCount(query, data.results[0]);
+    }
 
     // Optional: handle TMDB-specific error format if it ever applies
     if (data.Response === 'False') {
